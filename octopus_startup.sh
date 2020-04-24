6349155559a614021f9c9bddeb3bc6844efb5733
Anton Carter <antoncarter@fezmail.net>
2020-04-24 12:22:46 +0100

    added deploy scripts

---
+++ octopus_startup.sh
@@ -0,0 +1,27 @@
+cmd="up"
+octopus=0
+project_name="Merit"
+while getopts ":n:c:o" opt; do
+	case $opt in
+		o) octopus=1
+		;;
+		n) project_name="$OPTARG"
+		;;
+		c) cmd="$OPTARG"
+		;;
+		\?) echo "Invalid option -$OPTARG" >&2
+		;;
+	esac
+done
+
+if[[$cmd = "up"]]; then
+	if [[ octopus -eq 1]]; then
+		echo "Replacing '.env' file with 'octopus.env'"
+		mv ./.env ./.env.old
+		mv ./octopus.env ./.env
+	fi
+	docker-compose --project0name $project-name up -d 2>&1
+
+else
+	docker-compose --project0name $project-name $cmd -d 2>&1
+fi
